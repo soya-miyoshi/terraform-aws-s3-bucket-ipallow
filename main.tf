57dd61803b7efc338855a64f2a8fc921e262b9a4
Soya Miyoshi <soya.miyoshi@gmail.com>
2025-02-13 21:30:09 +0900

    modified statement

--- main.tf
+++ main.tf
@@ -506,9 +506,9 @@ data "aws_iam_policy_document" "bucket_policy" {
     for_each = length(var.source_ip_allow_list) > 0 ? [1] : []
 
     content {
-      sid       = "AllowIPPrincipals"
+      sid       = "DenyRequestsOutsideAllowedIPs"
       effect    = "Deny"
-      actions   = ["s3:*"]
+      actions   = ["s3:GetObject", "s3:ListBucket", "s3:PutObject"]
       resources = [local.bucket_arn, "${local.bucket_arn}/*"]
       principals {
         identifiers = ["*"]
